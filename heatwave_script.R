678b15dddda3ee8f102e5d5588077cd2820c4feb
angelaphan$ <angela7phan@gmail.com>
2025-09-04 10:08:37 -0400

    Created a script for a potential heatwave plot

---
+++ heatwave_script.R
@@ -0,0 +1,22 @@
+#Build a simple graph
+
+# Load packages
+library(ggplot2)
+library(EVR628tools)
+
+# Load data
+data("data_heatwaves")
+
+?data_heatwaves
+
+# Build a plot
+ggplot(data = data_heatwaves,
+       mapping = aes(x = year, y = mhw_int_cumulative,
+                     group = paste(fishery, eu_rnpa))) + geom_line()
+
+x <- ggplot(data = data_heatwaves,
+       mapping = aes(x = year, y = mhw_int_cumulative,
+                     group = paste(fishery, eu_rnpa))) + geom_line()
+
+x
+
